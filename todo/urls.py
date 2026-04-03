5f391a42339c2303267943c2bc86109412b5ac86
Nikola Vukosavljevic <vukosavljevicn.va@gmail.com>
2026-04-04 00:37:48 +0200

    Move all todo and auth routes to todo/urls.py and include them in config

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import TodoListCreateView, TodoDetailView
+from .views import TodoListCreateView, TodoDetailView, RegisterView, CurrentUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('todos/', TodoListCreateView.as_view()),
-    path('todos/<int:todo_id>/', TodoDetailView.as_view())
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('todos/', TodoListCreateView.as_view(), name='todo-list-create'),
+    path('todos/<int:todo_id>/', TodoDetailView.as_view(), name='todo-detail'),
+    path('users/me/', CurrentUserView.as_view(), name='users-me'),
 ]
