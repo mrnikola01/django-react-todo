32faee8b0dca4b5113e9cb23f585298ec6e417db
Nikola Vukosavljevic <vukosavljevicn.va@gmail.com>
2026-04-04 15:42:40 +0200

    Add BASE_URL from .env to API service

--- frontend/src/api/users.js
+++ frontend/src/api/users.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getCurrentUser = async () => {
   const token = localStorage.getItem("access");
