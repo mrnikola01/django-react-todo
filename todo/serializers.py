f2107e211f6d56185d7a4880b6a41c809532cbaf
Nikola Vukosavljevic <vukosavljevicn.va@gmail.com>
2026-04-05 17:03:34 +0200

    Update serializers.py

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
 from .models import Todo
+
+User = get_user_model()
 
 class TodoSerializer(serializers.ModelSerializer):
     owner_id = serializers.IntegerField(source='owner.id', read_only=True)
@@ -10,19 +11,22 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ['id', 'title', 'description', 'completed', 'owner_id', 'owner_username']
+        read_only_fields = ['id']
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
-    write_only=True,
-    min_length=6,
-    error_messages={
-        "min_length": "Password must be at least 6 characters long"
-    },
-)
+        write_only=True,
+        min_length=6,
+        error_messages={
+            "min_length": "Password must be at least 6 characters long"
+        },
+    )
 
     class Meta:
         model = User
         fields = ["id", "username", "password"]
+        read_only_fields = ["id"]
 
     def validate_username(self, value):
         if User.objects.filter(username=value).exists():
@@ -34,9 +38,10 @@ class RegisterSerializer(serializers.ModelSerializer):
             username=validated_data["username"],
             password=validated_data["password"],
         )
-    
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'is_staff']
+        read_only_fields = ['id', 'is_staff']
